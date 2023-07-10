ac707251afe7d9b7531f1ee16a21ddeffe736a43
shemajolivetgislain <shemajolivet@gmail.com>
2023-07-10 11:47:22 +0200

    union types

--- app.js
+++ app.js
@@ -5,20 +5,28 @@
 // const number2 = 2.8;
 // const result = add(number1, number2);
 // console.log(result);
-var role;
-(function (role) {
-    role[role["ADMIN"] = 0] = "ADMIN";
-    role[role["READ_ONLY"] = 1] = "READ_ONLY";
-    role[role["AUTHOR"] = 2] = "AUTHOR";
-})(role || (role = {}));
-;
-var person = {
-    name: 'shema',
-    city: 'kigali',
-    role: role.ADMIN
-};
-if (person.role === role.ADMIN) {
-    for (var role_1 in person) {
-        console.log(role_1);
+// enums in typescript
+// enum role {ADMIN, READ_ONLY, AUTHOR};
+// const person = {
+//     name:'shema',
+//     city:'kigali',
+//     role: role.ADMIN
+// }
+// if(person.role === role.ADMIN){
+//     for(const role in person){
+//         console.log(role.toUpperCase());
+//     }
+// }
+// union types
+function combine(input1, input2) {
+    if (typeof input1 === 'number' && typeof input2 === 'number') {
+        return input1 + input2;
+    }
+    else {
+        return input1.toString() + input2.toString();
     }
 }
+var result = combine(10, 40);
+console.log(result);
+var resultStrings = combine("shema", "gislain");
+console.log(resultStrings);
