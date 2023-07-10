9f312d375a16e4bfa1d7efe87b8bed56303c4d16
shemajolivetgislain <shemajolivet@gmail.com>
2023-07-10 14:31:19 +0200

    union types, alias

--- unionandalias.ts
+++ unionandalias.ts
@@ -5,8 +5,10 @@
 // const number2 = 2.8;
 // const result = add(number1, number2);
 // console.log(result);
+
 // enums in typescript
 // enum role {ADMIN, READ_ONLY, AUTHOR};
+
 // const person = {
 //     name:'shema',
 //     city:'kigali',
@@ -17,19 +19,25 @@
 //         console.log(role.toUpperCase());
 //     }
 // }
+
+
 // union types
-function combine(input1, input2) {
-    if (typeof input1 === 'number' && typeof input2 === 'number') {
+
+function combine(input1: number | string, input2: number| string){
+    if( typeof input1 === 'number' && typeof input2 === 'number'){
         return input1 + input2;
-    }
-    else {
+    }else{
         return input1.toString() + input2.toString();
     }
 }
-var result = combine(10, 40);
+
+const result= combine(10, 40);
 console.log(result);
-var resultStrings = combine("shema", "gislain");
+
+const resultStrings = combine("shema", "gislain");
 console.log(resultStrings);
-var u1 = { name: 'Max' };
+
+type User = { name: string } | string;
+let u1: User = {name: 'Max'};
 u1 = 'Michael';
 console.log("jolivet", u1);
