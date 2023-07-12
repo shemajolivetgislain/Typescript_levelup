28a410365a5d2d8302123311e3a9e4c04e0eb5ca
shemajolivetgislain <shemajolivet@gmail.com>
2023-07-12 16:01:11 +0200

    spread operator

---
+++ src/app.ts
@@ -0,0 +1,25 @@
+// const add = (a:number, b:number) => {
+//     return a+b;
+// }
+
+
+// const printResult = (output:number | string) =>{
+//     console.log(output);
+// }
+
+// printResult(add(3,5));
+// let sum=0;
+// const add = (...numbers:number[])=>{
+
+//   for (let index = 0; index < numbers.length; index++) {
+//     let sum=0;
+//     const nums = numbers[index];
+//     console.log(numbers.length)
+//     // console.log(index)
+//     // console.log(nums)
+//     sum=+numbers[index];
+//     console.log("sum is:", sum)
+//   }
+// }
+// add(1,2,9,8,7,3);
+// console.log(sum)
