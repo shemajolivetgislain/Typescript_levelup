16512af70e4bad6890182cf7d48275ab7c46858c
shemajolivetgislain <shemajolivet@gmail.com>
2023-07-13 08:58:01 +0200

    classed

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,13 @@
-// const add = (a:number, b:number) => {
-//     return a+b;
-// }
+class Department{
+    name: string;
+    constructor(n:string){
+        this.name = n;
+    }
+    describe(this:Department){
+        console.log("function Of Department", this.name);
+    }
+}
 
-
-// const printResult = (output:number | string) =>{
-//     console.log(output);
-// }
-
-// printResult(add(3,5));
-// let sum=0;
-// const add = (...numbers:number[])=>{
-
-//   for (let index = 0; index < numbers.length; index++) {
-//     let sum=0;
-//     const nums = numbers[index];
-//     console.log(numbers.length)
-//     // console.log(index)
-//     // console.log(nums)
-//     sum=+numbers[index];
-//     console.log("sum is:", sum)
-//   }
-// }
-// add(1,2,9,8,7,3);
-// console.log(sum)
+const objectDepts = new Department("Information Technology");
+console.log(objectDepts.describe)
+objectDepts.describe;
